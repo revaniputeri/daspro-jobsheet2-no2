39ad0b83c67dee2d3203bcee08b3bedc71d87815
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-02 17:23:37 +0700

    testnya fransiska

--- contoh.java
+++ contoh.java
@@ -1,7 +1,8 @@
-public static class contoh {
-    public static void main(String[] args) {
+public class contoh {
+
+  public static void main(String[] args) {
         System.out.println("hellow world");
         System.out.println("hellow world");
         System.out.println("test kevin");
-    }
-}
+    }  
+} 
