442e4502df55937d51e8413798505aed4cc5a580
Kevin <kevinadikasaputragithub@gmail.com>
2023-10-02 12:24:44 +0700

    :fire: Edit by kevin

--- contoh.java
+++ contoh.java
@@ -2,5 +2,6 @@ public static class contoh {
     public static void main(String[] args) {
         System.out.println("hellow world");
         System.out.println("hellow world");
+        System.out.println("test kevin");
     }
 }
